c1dadb6a285bba7e0ddadcb8579deac48d978f68
skye0402 <skyegu75@gmail.com>
2023-03-25 15:20:20 +0900

    Secured webservice, GET working. Before POST dev.

--- jco-rfc-rest-server/application/src/main/java/com/sap/apj/gunter/PurchaseRequisition.java
+++ jco-rfc-rest-server/application/src/main/java/com/sap/apj/gunter/PurchaseRequisition.java
@@ -1,9 +1,12 @@
 package com.sap.apj.gunter;
 
 import java.io.IOException;
-import java.io.PrintWriter; 
+import java.io.PrintWriter;
+import java.security.Principal;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.HttpConstraint;
+import javax.servlet.annotation.ServletSecurity;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -17,13 +20,27 @@ import org.slf4j.LoggerFactory;
 
 import com.sap.conn.jco.AbapException;
 import com.sap.conn.jco.JCoDestination;
-
 import com.sap.conn.jco.JCoDestinationManager;
 import com.sap.conn.jco.JCoException;
 import com.sap.conn.jco.JCoFunction;
 import com.sap.conn.jco.JCoParameterList;
 import com.sap.conn.jco.JCoStructure;
 import com.sap.conn.jco.JCoRepository;
+
+import com.sap.cloud.sdk.cloudplatform.security.AuthToken;
+import static com.sap.cloud.sdk.cloudplatform.security.AuthTokenAccessor.getCurrentToken;
+
+import com.sap.cloud.security.client.HttpClientException;
+import com.sap.cloud.security.client.HttpClientFactory;
+import com.sap.cloud.security.config.Environments;
+import com.sap.cloud.security.config.OAuth2ServiceConfiguration;
+import com.sap.cloud.security.token.AccessToken;
+import com.sap.cloud.security.token.TokenClaims;
+import com.sap.cloud.security.xsuaa.client.DefaultOAuth2TokenService;
+import com.sap.cloud.security.xsuaa.client.OAuth2TokenResponse;
+import com.sap.cloud.security.xsuaa.client.XsuaaDefaultEndpoints;
+import com.sap.cloud.security.xsuaa.tokenflows.XsuaaTokenFlows;
+import com.sap.cloud.security.token.*;
 
 
 /**
@@ -35,10 +52,26 @@ import com.sap.conn.jco.JCoRepository;
  */
 
 @WebServlet("/rfc/*")
+@ServletSecurity(@HttpConstraint(rolesAllowed = { "Display" }))
 public class PurchaseRequisition extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	private static final Logger logger = LoggerFactory.getLogger(PurchaseRequisition.class);
 	private static final String btpDestination = "S4HANA2021-RFC";
+	
+	private static XsuaaTokenFlows tokenFlows;
+	
+	@Override
+	public void init() throws ServletException {
+		OAuth2ServiceConfiguration configuration = Environments.getCurrent().getXsuaaConfiguration();
+
+		try {
+			tokenFlows = new XsuaaTokenFlows(
+					new DefaultOAuth2TokenService(HttpClientFactory.create(configuration.getClientIdentity())),
+					new XsuaaDefaultEndpoints(configuration), configuration.getClientIdentity());
+		} catch (HttpClientException e) {
+			throw new ServletException("Couldn't setup XsuaaTokenFlows");
+		}
+	}
 	
 	private JCoFunction getRfcFunction(JCoDestination btpDest, String fM) throws JCoException, RuntimeException {
 		// Prepare the call to the function module
@@ -72,6 +105,11 @@ public class PurchaseRequisition extends HttpServlet {
 		JSONObject jQuery = new JSONObject();
 		response.setContentType("application/json");
 		
+		// Get token
+		OAuth2TokenResponse tokenResponse = tokenFlows.clientCredentialsTokenFlow().execute();
+		logger.info("Access-Token-Payload:" + tokenResponse.getDecodedAccessToken().getPayload());
+		
+
 		// Get request parameters
 		String functionName = request.getParameter("fm");
 		logger.info("Function module: " + functionName);
@@ -87,8 +125,8 @@ public class PurchaseRequisition extends HttpServlet {
             return;
 		}
 		
-		// Function module call preparation
 		try {
+			// Function module call preparation
 			JCoDestination btpDest = JCoDestinationManager.getDestination(btpDestination);	
 			JCoFunction rfcFunction = getRfcFunction(btpDest, functionName);
 			JCoParameterList imports = rfcFunction.getImportParameterList();
@@ -109,6 +147,8 @@ public class PurchaseRequisition extends HttpServlet {
 			JSONObject jTables = new JSONObject(tables.toJSON());
 			jsonResponse.put("tables", jTables);
             
+			// Success, respond with JSON result
+			logger.info("Sending JSON result from BAPI call...");
             response.setContentType("application/json");
             response.getWriter().write(jsonResponse.toString());
             
@@ -120,6 +160,7 @@ public class PurchaseRequisition extends HttpServlet {
 			jsonResponse.put("errormessage", e.getMessage());
 			response.setStatus(400);
             response.getWriter().write(jsonResponse.toString());
+            logger.error("ABAP FM exception: " + e.getMessage());
             return;
 		}
 		catch (JCoException e) {
@@ -127,6 +168,7 @@ public class PurchaseRequisition extends HttpServlet {
 			jsonResponse.put("errormessage", e.getMessage());
 			response.setStatus(400);
             response.getWriter().write(jsonResponse.toString());
+            logger.error("JCo exception: " + e.getMessage());
             return;
 		}
 		catch (RuntimeException e) {
@@ -134,6 +176,7 @@ public class PurchaseRequisition extends HttpServlet {
 			jsonResponse.put("errormessage", e.getMessage());
 			response.setStatus(400);
             response.getWriter().write(jsonResponse.toString());
+            logger.error("Runtime exception: " + e.getMessage());
             return;
 		}
 	}
